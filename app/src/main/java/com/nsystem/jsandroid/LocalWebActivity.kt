9b6b5fd15cb209a0ea465cf4918f512965372107
Putra Nugraha <putra.nugraha@dana.id>
2020-10-17 20:35:47 +0700

    Show notification & toast on JS call txtFromWeb function

--- app/src/main/java/com/nsystem/jsandroid/LocalWebActivity.kt
+++ app/src/main/java/com/nsystem/jsandroid/LocalWebActivity.kt
@@ -1,11 +1,20 @@
 package com.nsystem.jsandroid
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.webkit.JavascriptInterface
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import kotlinx.android.synthetic.main.activity_local_web.*
 
 class LocalWebActivity : AppCompatActivity() {
@@ -15,6 +24,12 @@ class LocalWebActivity : AppCompatActivity() {
         private const val ANDROID_NSYSTEM = "AndroidNSystem"
 
         private const val WEBVIEW_URL = "file:///android_asset/webview.html"
+
+        private const val NOTIFICATION_CHANNEL_ID = "Nsystem_Channel"
+
+        private const val NOTIFICATION_TICKER = "Nsystem Notification Ticker"
+
+        private const val NOTIFICATION_NAME = "Nsystem Notification"
     }
 
     @SuppressLint("SetJavaScriptEnabled")
@@ -47,7 +62,61 @@ class LocalWebActivity : AppCompatActivity() {
 
         @JavascriptInterface
         fun textFromWeb(fromWeb: String) {
-            txtFromWeb.text = fromWeb
+            showNotification(fromWeb)
+
+            /**
+             *  Should use runOnUiThread for UI component,
+             *  without runOnUiThread code below UI component code will not be called
+             *  These happened due to javascript interface run in new thread (not in UI thread)
+             */
+            runOnUiThread {
+                txtFromWeb.text = fromWeb
+            }
+            Toast.makeText(this@LocalWebActivity, fromWeb, Toast.LENGTH_LONG).show()
+        }
+
+        private fun showNotification(fromWeb: String) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                showNotificationForOreoAndHigher(fromWeb)
+            }
+            showNotificationForBelowOreo(fromWeb)
+        }
+
+        @RequiresApi(Build.VERSION_CODES.O)
+        private fun showNotificationForOreoAndHigher(fromWeb: String) {
+            @SuppressLint("WrongConstant") val notificationChannel =
+                NotificationChannel(
+                    NOTIFICATION_CHANNEL_ID,
+                    NOTIFICATION_NAME,
+                    NotificationManager.IMPORTANCE_MAX
+                ).apply {
+                    description = fromWeb
+                    enableLights(true)
+                    lightColor = Color.RED
+                    vibrationPattern = longArrayOf(0, 1000, 500, 1000)
+                    enableVibration(true)
+                }
+
+            val notificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+
+        private fun showNotificationForBelowOreo(fromWeb: String) {
+            val notificationBuilder =
+                NotificationCompat.Builder(this@LocalWebActivity, NOTIFICATION_CHANNEL_ID)
+            notificationBuilder.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setTicker(NOTIFICATION_TICKER) //.setPriority(Notification.PRIORITY_MAX)
+                .setContentTitle(NOTIFICATION_NAME)
+                .setContentText(fromWeb)
+                .setContentInfo(fromWeb)
+
+            val notificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.notify(1, notificationBuilder.build())
         }
     }
 }
